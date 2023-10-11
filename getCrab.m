2ccc5ec404aa826d9ec81bca2d3e669587050d07
dpc43 <dpc43@humboldt.edu>
2023-10-11 13:22:12 -0700

    Create getCrab.m

---
+++ getCrab.m
@@ -0,0 +1,12 @@
+## Author: Dean Callahan <deancallahan@Deans-MacBook-Pro.local>
+## Created: 2023-08-30
+function crab = getCrab (crabSize)
+
+%Crab test
+crabPt1=[crabSize;crabSize;1]
+crabPt2=[-crabSize;crabSize;1]
+
+
+%captain matrix
+crab = [ crabtPt1, crabPt2];
+endfunction
